912bf86d40b692baeed501cbee60b1cfb0138b75
qayyamj <qayyamj@gmail.com>
2021-03-22 13:38:36 -0400

    log route for testing

--- Scripts/router.js
+++ Scripts/router.js
@@ -101,6 +101,8 @@ router.AddTable(["/",
                 
 let route = location.pathname; // alias for location.pathname
 
+console.log(route);
+
 if(router.Find(route) > -1)
 {
     router.ActiveLink = (route == "/") ? "home" : route.substring(1)
