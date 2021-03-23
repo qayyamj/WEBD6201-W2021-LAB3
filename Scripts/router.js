2b31b0e8deff9372f72a2913e1f3fa7832e0ae2a
qayyamj <qayyamj@gmail.com>
2021-03-23 15:46:40 -0400

    fixed remaining bugs

--- Scripts/router.js
+++ Scripts/router.js
@@ -101,8 +101,6 @@ router.AddTable(["/",
                 
 let route = location.pathname; // alias for location.pathname
 
-console.log(route);
-
 if(router.Find(route) > -1)
 {
     router.ActiveLink = (route == "/") ? "home" : route.substring(1)
